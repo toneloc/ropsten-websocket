011dc16e08328b1322bc3c2d1e747be34fc7f166
Tony Klausing <toneloc@users.noreply.github.com>
2018-04-16 20:59:00 -0500

    with claim ether function added

--- ropsten-websocket.js
+++ ropsten-websocket.js
@@ -1,3 +1,4 @@
+// "server-side schtuff"
 window.LogData = function getData() { 
 	results = [{}];
 
@@ -6,7 +7,7 @@ window.LogData = function getData() {
 	const web3 = new Web3(new Web3.providers.WebsocketProvider('wss://ropsten.infura.io/ws'));
 	var abi = [ { "constant": false, "inputs": [ { "name": "_username", "type": "string" } ], "name": "createTweetWallet", "outputs": [ { "name": "", "type": "bool" } ], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "anonymous": false, "inputs": [ { "indexed": false, "name": "_username", "type": "string" }, { "indexed": false, "name": "_address", "type": "address" } ], "name": "TweetWalletCreated", "type": "event" } ];
 
-	var contract = new web3.eth.Contract(abi,'0xf396316bD6DE45Dd85F8C465715798Bf13C46E26');
+	var contract = new web3.eth.Contract(abi,'0x7affb490fec3793ab370dfbd45db4cd9f9f3f6b9');
 
 	contract.getPastEvents('allEvents',
 	    {fromBlock: 0,  toBlock: 'latest'},
@@ -54,3 +55,88 @@ window.GetBalances = function getData(addresses) {
   	});
 }
 
+window.CreateTweetWallet = function createTweetWallet(username) { 
+	results = [{}];
+
+	console.log(username);
+
+	var web3;
+
+	const Web3 = require('web3');
+	    if (typeof web3 !== 'undefined') {
+	        web3 = new Web3(web3.currentProvider);
+	    } else {
+	        App.web3Provider = new Web3(window.web3.currentProvider);
+	        web3 = new Web3(App.web3Provider);
+	    }
+
+    web3.eth.getAccounts(function (error, accounts) {
+         if (error) {
+            alert('It looks like you are not logged into the MetaMask browser plugin. To use TweetTip.me, please connect to the Ethereum network with Metamask, metamask.io');
+        }
+
+        if (accounts.length == 0) {
+            alert('Please connect to the Ethereum mainnet with the MetaMask browser extension, or use a service such as MyEtherWallet');
+            return;
+        } else {
+            var account = accounts[0];
+        }
+
+        var account = accounts[0];
+		var abi = [ { "constant": false, "inputs": [ { "name": "_username", "type": "string" } ], "name": "createTweetWallet", "outputs": [ { "name": "", "type": "bool" } ], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "anonymous": false, "inputs": [ { "indexed": false, "name": "_username", "type": "string" }, { "indexed": false, "name": "_address", "type": "address" } ], "name": "TweetWalletCreated", "type": "event" } ];
+		var contract = new web3.eth.Contract(abi,'0x7affb490fec3793ab370dfbd45db4cd9f9f3f6b9', {from: account});
+
+        contract.methods.createTweetWallet(username).send({from: account})
+			.on('error', function(error){ alert('Your transaction had an error :( Here is some more information - ' +  error); })
+			.on('transactionHash', function(transactionHash){ alert('Your transaction is processing with transaction ID - ' +  transactionHash);})
+			// .on('receipt', function(receipt){ alert('Your Tip has been registered on the blockchain at this contract address: ' + receipt.contractAddress) })
+			.then(function(newContractInstance){
+			    console.log(newContractInstance.options.address) 
+			});
+    });
+
+}
+
+window.ClaimEther = function claimEther(statusID, contractAddress) { 
+	results = [{}];
+
+	var web3;
+
+	const Web3 = require('web3');
+	    if (typeof web3 !== 'undefined') {
+	        web3 = new Web3(web3.currentProvider);
+	    } else {
+	        App.web3Provider = new Web3(window.web3.currentProvider);
+	        web3 = new Web3(App.web3Provider);
+	    }
+
+    web3.eth.getAccounts(function (error, accounts) {
+         if (error) {
+            alert('It looks like you are not logged into the MetaMask browser plugin. To use TweetTip.me, please connect to the Ethereum network with Metamask, metamask.io');
+        }
+
+        if (accounts.length == 0) {
+            alert('Please connect to the Ethereum mainnet with the MetaMask browser extension, or use a service such as MyEtherWallet');
+            return;
+        } else {
+            var account = accounts[0];
+        }
+
+        var account = accounts[0];
+
+        var abi = [{"constant":false,"inputs":[{"name":"myid","type":"bytes32"},{"name":"result","type":"string"}],"name":"__callback","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"myid","type":"bytes32"},{"name":"result","type":"string"},{"name":"proof","type":"bytes"}],"name":"__callback","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"username","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"getUsername","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"refundOwner","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_statusId","type":"string"}],"name":"claim","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[{"name":"_username","type":"string"}],"payable":true,"stateMutability":"payable","type":"constructor"},{"payable":true,"stateMutability":"payable","type":"fallback"},{"anonymous":false,"inputs":[{"indexed":false,"name":"description","type":"string"}],"name":"newOraclizeQuery","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"tweetInfo","type":"string"}],"name":"newTweetInfo","type":"event"}];
+            
+        var contract = new web3.eth.Contract(abi,contractAddress, {from: account});
+
+        // test status = 985942467995369473
+        contract.methods.claim(status).send({from: account})
+            .on('error', function(error){ alert('Your transaction had an error :( Here is some more information - ' +  error); })
+            .on('transactionHash', function(transactionHash){ alert('Your transaction is processing with transaction ID - ' +  transactionHash);})
+            .then(function(newContractInstance){
+                console.log("got here") 
+                $('#claimModal').modal('toggle');
+                $('#balances-table').find("tr:gt(0)").remove();
+            });
+    });
+	
+}
